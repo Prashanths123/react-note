29d4738d7fe892e9da2452cd0c2e01902dbc1369
unknown <ssprashanth022@gmail.com>
2024-02-16 10:06:14 +0530

    done

---
+++ pronote/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import EditorClass from './components/Editor';
+function App() {
+  return (
+    <div className="App">
+      <EditorClass/>
+    </div>
+  );
+}
+
+export default App;
